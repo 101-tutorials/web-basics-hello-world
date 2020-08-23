2fb6dadc4f1487df110ed3538f0587410cc2e437
Prae Songprasit <prae.songprasit@xero.com>
2020-08-23 17:35:34 +1200

    Fix API and make loading state default

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
     // document ready
-    console.log("DOM loaded...");
+    // console.log("DOM loaded...");
     
     const generateTriviaButton = document.querySelector(`#js-button`);
     const loadingVisual = document.querySelector("#js-loadingVisual");
@@ -12,68 +12,36 @@ document.onreadystatechange = () => {
     function showAnimalTrivia() {
       // the display area will show "loading" until it is changed by displayQuestionAndAnswer
       // once the API call has finished
-      console.log("clicked");
+      // console.log("clicked");
       fetchRandomTriviaQuestion(displayQuestionAndAnswer);
     }
     
     function displayQuestionAndAnswer(question, answer) {
+      loadingVisual.classList.remove('loading-animation');
+      loadingVisual.classList.add('is-hidden');
       resultsArea.innerHTML = "<p>True or false?</p>";
       document.querySelector("#js-questionDisplay").innerHTML = `<p> ${question}</p>`;
       document.querySelector("#js-answerDisplay").innerHTML = `<p>Answer: ${answer}</p>`;
-      generateTriviaButton.textContent = "Show me another one";
+      generateTriviaButton.textContent = "Show me another trivia";
     }
-    
-    // https://javascript.info/xmlhttprequest
-    // Gets a random animal trivia question from an API, then pass the result to the callback function
-    function fetchRandomTriviaQuestion(callback) {
+
+    // https://javascript.info/promise-api
+    // Configure your own api call at https://opentdb.com/api_config.php
+    function fetchRandomTriviaQuestion(callback) {      
       // This API gets random trvia questions
       // The url includes parameters to configure the API to only return
       // true or false trivia on animals encoded in base64
+      const fetchPromise = fetch("https://opentdb.com/api.php?amount=1&category=27&type=boolean&encode=base64");
       
-      // 1. Create a new XMLHttpRequest object
-      // Configure your own api call at https://opentdb.com/api_config.php
-      let request = new XMLHttpRequest();
-      let url = new URL('https://opentdb.com/api.php');
-          url.searchParams.set('amount', '1');
-          url.searchParams.set('category', '27');
-          url.searchParams.set('type', 'boolean');
-          url.searchParams.set('encode', 'base64');
-
-      // request.open('GET', 'https://opentdb.com/api.php?amount=1&category=27&type=boolean&encode=base64');
-      request.open('GET', url);
-      
-      request.responseType = 'json';
-
-      request.send();
-
-      //not getting right data
-      request.onload = (data) => {
-        // loadingVisual.classList.add('is-hidden');
-        // loadingVisual.classList.remove('oading-animation');
-        // Success!
-        console.log(request);
-        console.log(data);
-  
-        var result = data.results;
-  
-        // atob() is a built in method to decode base64 encoded strings
+      fetchPromise.then(response => {
+        return response.json();
+      }).then(results => {
+        let result = results.results;
         let question = atob(result[0].question);
         let answer = atob(result[0].correct_answer);
-    
-        // call the function we passed into fetchRandomTriviaQuestion
-        callback(question, answer);
-      };
 
-      request.onprogress = () => {
-        loadingVisual.classList.add('loading-animation');
-        loadingVisual.classList.remove('is-hidden');
-      };
-    
-      request.onerror = () => {
-        // There was a connection error of some sort
-        console.log("Request failed");
-      };
+        callback(question, answer);
+      });
     }
   }
 };
-
